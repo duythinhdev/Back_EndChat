dbea2e24187a4151f1e5cc05bda3c494269df73f
DESKTOP-LSPDP1F\Thinh <thinh.do@fdssoft.com>
2021-06-10 14:13:05 +0700

    change all file

--- app.js
+++ app.js
@@ -30,6 +30,16 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true})
 //     );
 //     next();
 // });
+app.use((req,res,next) => {
+res.header('Access-Control-Allow-Origin','*');
+res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With,Content-Type,Accept,Authorization');
+if(req.method === "OPTIONS")
+{
+    req.header('Access-Control-Allow-Methods','PUT,POST,PATH,DELETE,GET');
+    res.status(200).json({message: "cors success"});
+}
+})
 mongoose.Promise = global.Promise;
 
 app.use(cors({
